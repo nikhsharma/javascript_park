de83f7e193d54d578777027de6b273076a72163f
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-05 17:27:40 +0100

    Can add a dinosaur to a parks enclosure

--- park.js
+++ park.js
@@ -2,4 +2,8 @@ const Park = function() {
   this.enclosure = [];
 }
 
+Park.prototype.addDinosaur = function(dinosaur) {
+  this.enclosure.push(dinosaur);
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,16 +1,24 @@
 const assert = require('assert');
 const Park = require('../park.js');
+const Dinosaur = require('../dinosaur.js')
 
 describe('Park', function() {
 
   let park;
+  let dinosaur;
 
   beforeEach(function() {
     park = new Park();
+    dinosaur = new Dinosaur('Velociraptor', 3);
   });
 
   it('should start with an empty enclosure', function() {
     assert.strictEqual(park.enclosure.length, 0);
-  })
+  });
+
+  it('should be able to add a dinosaur to the enclosure', function() {
+      park.addDinosaur(dinosaur);
+      assert.strictEqual(park.enclosure.length, 1);
+  });
 
 })
